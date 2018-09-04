90e28d601339d3b5deede18fb5b0f01adc54d6cc
CaveMatthewLC <matthew.cave@ladbrokescoral.com>
2018-09-04 12:08:17 +0100

    Add project files.

---
+++ HelloDockerizedWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloDockerizedWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string output = "Hello Dockerized World";
+
+			Console.WriteLine(output);
+			
+			Logger.Warn("Warning: this may be running inside a Docker container");
+		}
+	}
+}
